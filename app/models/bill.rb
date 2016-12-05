2e817e374881ca970737778493bddddb4cfa37ff
Nazar Matus <funkyloverone@gmail.com>
2016-12-05 17:53:18 +0200

    fix bill statuses to not override #open

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -2,5 +2,5 @@ class Bill < ApplicationRecord
   belongs_to :loaner, class_name: 'User'
   belongs_to :debtor, class_name: 'User'
 
-  enum status: [:new_bill, :open, :declined, :pending, :paid, :reopen]
+  enum status: [:new_bill, :open_bill, :declined, :pending, :paid, :reopen]
 end
