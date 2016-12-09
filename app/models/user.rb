2b5558233b9955379f810660c82d87c3ff470450
Nazar Matus <funkyloverone@gmail.com>
2016-12-09 20:26:47 +0200

    add username to sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,6 @@ class User < ApplicationRecord
                     styles: { medium: '300x300>', thumb: '100x100>' },
                     default_url: '/assets/missing.jpg'
   validates_attachment_content_type :avatar, content_type: %r{\Aimage/.*\z/}
+
+  validates :username, presence: true
 end
